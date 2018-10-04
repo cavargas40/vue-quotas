5cfe4fe37544fe5fd1ecef544a4433e73631abdf
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2018-10-05 00:13:12 +1300

    added quotas development for see the correct use of components

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Quotes Added</h3>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" arial-valuenow="60" arial-valuemin="0" aria-valuemax="100"
+            :style="{ width: (quoteCount / maxQuotes) * 100 + '%' }">
+        {{ quoteCount }} / {{ maxQuotes }}
+        {{ (quoteCount / maxQuotes) * 1000 + '%' }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quoteCount', 'maxQuotes'],
+}
+</script>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, index) in quotes" v-bind:key="index" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+  </div>
+</template>
+<script>
+import appQuote from './Quote.vue'
+
+export default {
+  props: ['quotes'],
+  components: {
+    appQuote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit('quoteDeleted', index)
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
